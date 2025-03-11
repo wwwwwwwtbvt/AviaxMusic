26f699abf538663d239f8fa16ffe300ff2cf2d2d
wwwwwwwtbvt <alifarhan306539@gmail.com>
2025-03-11 10:56:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("29944858"))
+API_HASH = getenv("f401a8350f36a3ee90ee5ac99fb41e93")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("8086660602:AAGMvF1aDGmnIjJpMv4uh9eFmlaDIuiLNIY")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -22,13 +22,13 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
 # Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", None))
+OWNER_ID = int(getenv("7523858033", None))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_API_KEY = getenv("HRKU-619f0f7f-6e27-4577-aaec-19dba409079c")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexGenBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NexGenSpam")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/CodeDynamos")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+7yH0720sRXYxMzM1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -65,7 +65,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION", BQHI7BoAnhwOO_VC8dSc-ECf2L7rPHdbUUhV7ZA25jL_JzOQlYmeIuxYwJ8XvJrA4dqHbo--D-7jMqZRmAyiA0s5WLdUW6QTmApJkU7Zvwv19hs_CdtEtwtIJC6WRIubM9hlEm2agBTNt89BMKNU5loWOU3P2-o4IAoFQuAM9K8MLsNA8caGu7Ldbu-4JAzjk-QG6hLUDo5N_oH-a_RG6ir_Ssx51dsVTDdVDzoZRjG03Dgwzvy_aV8upCub2qGbudJlDEs_xWjT-CCAOm0LeTUZklOqOIGUkLhlLxElQS9oB08HzPvMTJwfJLBYJ_S383_KZFN4cnU8B4Zc9Kkb3vBubuUu_QAAAAHiAKX6AQ)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -107,7 +107,7 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
 if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+    if not re.match("(?:https://t.me/+7yH0720sRXYxMzM1", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
